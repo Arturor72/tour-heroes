99b87201471dedbec387b480ef645b10204f1413
arturo <arturor72.jar@gmail.com>
2019-03-08 16:37:35 -0500

    finish app tour

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero'; 
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  
+  heroes:Hero[]=[];
+
+  constructor(private heroServices: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroServices.getHeroes().subscribe(heroes => this.heroes=heroes.slice(1,5));
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[]=[
+    {id: 11, name:'Lion'},
+    {id: 12, name:'Axe'},
+    {id: 13, name:'Lina'},
+    {id: 14, name:'Dazzle'},
+    {id: 15, name:'Storm Spirit'},
+    {id: 16, name:'Earth Spirit'},
+    {id: 17, name:'Ember Spirit'},
+    {id: 18, name:'Spirit Breaker'},
+    {id: 19, name:'Magnus'},
+    {id: 20, name:'Bane'}
+];
